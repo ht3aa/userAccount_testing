ba8f167426b8c62c1c7077e8e8b6a5a5c7f02df1
ht3aa <ht3aa2001@gmail.com>
2023-10-26 15:35:21 +0300

    Adding tests for files

---
+++ src/tests/unit-tests/userAccount.test.ts
@@ -0,0 +1,47 @@
+const fs = require("fs").promises;
+const path = require("path");
+const folderPath = path.join(__dirname, "../../services");
+
+const createUserFile = path.join(folderPath, "createUser.ts");
+const updateUserFile = path.join(folderPath, "updateUser.ts");
+const deleteUserFile = path.join(folderPath, "deleteUser.ts");
+const getUserFile = path.join(folderPath, "getUser.ts");
+const getUsersFile = path.join(folderPath, "getUsers.ts");
+
+describe("user account service files", () => {
+  it(`should check if "${createUserFile}" exists in "src/services" directory`, async () => {
+    try {
+      await fs.access(createUserFile);
+    } catch (err) {
+      expect(err).toBe(null);
+    }
+  });
+  it(`should check if "${updateUserFile}" exists in "src/services" directory`, async () => {
+    try {
+      await fs.access(updateUserFile);
+    } catch (err) {
+      expect(err).toBe(null);
+    }
+  });
+  it(`should check if "${deleteUserFile}" exists in "src/services" directory`, async () => {
+    try {
+      await fs.access(deleteUserFile);
+    } catch (err) {
+      expect(err).toBe(null);
+    }
+  })
+  it(`should check if "${getUserFile}" exists in "src/services" directory`, async () => {
+    try {
+      await fs.access(getUserFile);
+    } catch (err) {
+      expect(err).toBe(null);
+    }
+  })
+  it(`should check if "${getUsersFile}" exists in "src/services" directory`, async () => {
+    try {
+      await fs.access(getUsersFile);
+    } catch (err) {
+      expect(err).toBe(null);
+    }
+  })
+});
