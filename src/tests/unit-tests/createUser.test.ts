1c61feffd913c8ff4de484f0310ed3c08297f3d6
HusseinMansourIQ <70377627+HusseinMansourIQ@users.noreply.github.com>
2023-10-26 19:54:34 +0300

    added phone number/email

--- src/tests/unit-tests/createUser.test.ts
+++ src/tests/unit-tests/createUser.test.ts
@@ -6,6 +6,8 @@ describe("Create User Post request", () => {
   it(`should check if "createUserFile" exists in "src/services" directory`, async () => {
     const response = await request(app).post("/CreateUser").send({ 
         userName: "TestUserName", 
+        PhoneNumber : "testPhoneNumber",
+        Email : "testEmail",
         password: "TestPassword" 
       }).set('Accept', 'application/json')
 
